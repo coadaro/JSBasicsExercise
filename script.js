d702d70a8121e5fe636d40eddbac77a0bf4f0cfd
“coadaro” <coadaro@gmail.com>
2022-02-28 16:41:14 -0500

    JSBasicsExercise

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log(4+4);
+//numbers, strings, booleans, null, undefined
+//+, -, *, /, **, %
+console.log(5%2);
+console.log(1%5);
+console.log('I\'M MARY POPPINS, Y\'ALL');
+console.log('Hawk'+'eye');
+console.log('cory'.length+'roberts'.length);
+console.log('Avengers: Endgame'[16]);
+
+//bonus
+console.log('\\');
+console.log(5**4-9);
