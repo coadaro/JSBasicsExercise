487c9b44bcac352bcde9fe535814c24979289bfa
“coadaro” <coadaro@gmail.com>
2022-03-01 10:17:39 -0500

    JSBasicsExerciseWithBonus

--- script.js
+++ script.js
@@ -2,12 +2,29 @@ console.log(4+4);
 //numbers, strings, booleans, null, undefined
 //+, -, *, /, **, %
 console.log(5%2);
-console.log(1%5);
-console.log('I\'M MARY POPPINS, Y\'ALL');
+console.log(5%4);
+console.log('I\'M MARY POPPINS, Y\'ALL!');
 console.log('Hawk'+'eye');
 console.log('cory'.length+'roberts'.length);
 console.log('Avengers: Endgame'[16]);
 
 //bonus
 console.log('\\');
+
 console.log(5**4-9);
+
+console.log('I'.length+'AM'.length%'IRON'.length-'MAN'.length);
+
+console.log('CAP'[1]);
+console.log('VISION'[0]);
+console.log('OKOYE'[4]);
+console.log('NICK FURY'[0]);
+console.log('GROOT'[0]);
+console.log('DOCTOR STRANGE'[13]);
+console.log('THOR'[3]);
+console.log('STAR-LORD'[0]);
+
+let m=25;
+let c=4;
+let u=m+c;
+console.log(u);
